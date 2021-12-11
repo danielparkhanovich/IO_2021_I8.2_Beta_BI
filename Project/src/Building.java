cce552a783938d81e4d17c3736a7ae0076754b46
Daniel Parkhanovich <daniel.parkhanovich@gmail.com>
2021-12-11 22:19:03 +0200

    Added base project structure.

---
+++ Project/src/Building.java
@@ -0,0 +1,17 @@
+
+
+
+public class Building extends Location {
+
+    private Floor[] floors;
+
+    public Building(int id, Floor[] floors) {
+        super(id);
+        this.floors = floors;
+    }
+
+    public Building(int id, String name, Floor[] floors) {
+        super(id, name);
+        this.floors = floors;
+    }
+}
---
+++ Project/src/Room.java
@@ -0,0 +1,20 @@
+
+
+
+public class Room extends Location {
+
+    private float area;
+    private float cube;
+    private float heating;
+    private float light;
+
+    public Room(int id) {
+        super(id);
+    }
+
+    public Room(int id, String name) {
+        super(id, name);
+    }
+
+
+}
